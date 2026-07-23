dee78664498af0cc507ea6468e9dec3b2afb8b61
Qinwen Yan <769423645@qq.com>
2026-07-23 21:18:35 +0800

    update the framework of the code in cmake

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -3,10 +3,14 @@
 
 
 /* 提供一个本地的简单服务*/
-class UserService{
-    UserService() = default; 
+class UserService { //使用rpc服务发布端（rpc服务提供者）
     void login(std::string name, std::string pwd){
         std::cout << "Doing the local service: login..."<< std::endl; 
         std::cout << "Name: " << name << " pwd: " << pwd << std::endl; 
     }
+    
 }; 
+
+int main(){
+
+}
