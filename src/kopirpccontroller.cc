dc69aabb0b02945ad1080e6b5967bb5f7cb0c36f
Qinwen Yan <769423645@qq.com>
2026-08-01 19:14:22 +0000

    implement the whole controller

--- src/kopirpccontroller.cc
+++ src/kopirpccontroller.cc
@@ -14,3 +14,9 @@ void KopirpcController::SetFailed(const std::string& reason) {
   isFailed = true;
   errText = reason;
 }
+
+//目前为实现的具体功能：现在用不到，直接置为空函数
+void KopirpcController::StartCancel(){}
+bool KopirpcController::IsCandeled() const {return false;}
+void NotifyOnCancel(google::protobuf::Closure* callback){}
+
