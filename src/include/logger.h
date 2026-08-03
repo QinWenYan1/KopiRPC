1dbd1d2acc491cf26cd1247d4aaa8c5387ce706f
Qinwen Yan <769423645@qq.com>
2026-08-03 03:00:07 +0000

    complete the structure of logger api

--- src/include/logger.h
+++ src/include/logger.h
@@ -6,9 +6,18 @@ enum LogLevel{
     INFO, //普通信息
     ERROR,//错误信息
 }; 
-//kopirpc框架提供的日志系统
+//kopirpc框架提供的日志系统：使用单例模式
 class Logger{
 public:
+    //设置日志级别
+    void SetLogLevel(LogLevel level); 
+    //写日志
+    void Log(std::string msg); 
+private:
     int loglevel; //记录日志级别
-    LockQueue<std::string> logQueue; 
+    LockQueue<std::string> logQueue; //日志缓冲队列
+
+    Logger() = default; 
+    Logger(const Logger&) = delete; 
+    Logger(Logger&&) = delete; 
 }; 
