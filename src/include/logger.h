560a1e92a66a4adfabab5af25a21b8cb9f14098e
Qinwen Yan <769423645@qq.com>
2026-08-06 15:33:13 +0000

    rewrite the macro

--- src/include/logger.h
+++ src/include/logger.h
@@ -1,48 +1,49 @@
-#pragma once 
-#include "lockqueue.h"
+#pragma once
 #include <string>
 
-enum LogLevel{
-    INFO, //普通信息
-    ERROR,//错误信息
-}; 
-//kopirpc框架提供的日志系统：使用单例模式
-class Logger{
-public:
-    //获取日志的单例
-    static Logger& GetInstance(); 
-    //设置日志级别
-    void SetLogLevel(LogLevel level); 
-    //写日志
-    void Log(std::string msg); 
-private:
-    int loglevel; //记录日志级别
-    LockQueue<std::string> logQueue; //日志缓冲队列
+#include "lockqueue.h"
 
-    Logger(); 
-    Logger(const Logger&) = delete; 
-    Logger(Logger&&) = delete; 
-}; 
+enum LogLevel {
+  INFO,   //普通信息
+  ERROR,  //错误信息
+};
+// kopirpc框架提供的日志系统：使用单例模式
+class Logger {
+ public:
+  //获取日志的单例
+  static Logger& GetInstance();
+  //设置日志级别
+  void SetLogLevel(LogLevel level);
+  //写日志
+  void Log(std::string msg);
 
+ private:
+  int loglevel;                     //记录日志级别
+  LockQueue<std::string> logQueue;  //日志缓冲队列
 
-//定义日志宏给用户更方便的方式去日志写入LOG_INFO("xx %d %s", 20, "xxxx")
-#define LOG_INFO(logmsgformat, ...) \
-    do  \
-    {   \
-        Logger& logger = Logger::GetInstance();\
-        logger.SetLogLevel(INFO);\
-        char c[1024] = {0}; \
-        snprintf(c,1024,logmsgformat, ##__VA_ARGS__); \
-        logger.Log(c) \
-    } while(0); 
+  Logger();
+  Logger(const Logger&) = delete;
+  Logger(Logger&&) = delete;
+};
 
+// 定义日志宏给用户更方便的方式去日志写入，例如：LOG_INFO("xx %d %s", 20,
+// "xxxx")
+#define LOG_INFO(logmsgformat, ...)                 \
+  do {                                              \
+    Logger& logger = Logger::GetInstance();         \
+    logger.SetLogLevel(INFO);                       \
+    char c[1024] = {0};                             \
+    snprintf(c, 1024, logmsgformat, ##__VA_ARGS__); \
+    logger.Log(c);                                  \
+  } while (0)
 
-#define LOG_ERR(logmsgformat, ...) \
-    do  \
-    {   \
-        Logger& logger = Logger::GetInstance();\
-        logger.SetLogLevel(ERROR);\
-        char c[1024] = {0}; \
-        snprintf(c,1024,logmsgformat, ##__VA_ARGS__); \
-        logger.Log(c) \
-    } while(0); 
+#define LOG_ERR(logmsgformat, ...)                  \
+  do {                                              \
+    Logger& logger = Logger::GetInstance();         \
+    logger.SetLogLevel(ERROR);                      \
+    char c[1024] = {0};                             \
+    snprintf(c, 1024, logmsgformat, ##__VA_ARGS__); \
+    logger.Log(c);                                  \
+  } while (0)
+// ⚠️ 关键：请确保上面这行 while(0)
+// 敲完回车后，下面还有一行空行（即文件以换行符结束）
