00aed582348bf5f8f2922b42a5724ac906a367e6
Qinwen Yan <769423645@qq.com>
2026-08-07 03:57:02 +0000

    implement the whole two functions

--- src/include/lockqueue.h
+++ src/include/lockqueue.h
@@ -8,6 +8,7 @@
 template <typename T>
 class LockQueue {
  public:
+    //多个worker线程都会写日志queue
     void Push(const T& data){
         //加锁
         std::lock_guard<std::mutex> lock(mtx); 
@@ -16,7 +17,9 @@ class LockQueue {
         //锁自动释放,由于写日志线程只有一个就只需要notify one 
         conVar.notify_one(); 
     }   
-    T&  Pop(){
+
+    //只有一个线程会阅读日志queue
+    T  Pop(){
         std::unique_lock<std::mutex> lock(mtx); 
         while (q.empty()){
             //日志队列为空，线程进入wait状态
