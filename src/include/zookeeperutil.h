3d14821d3af8df2196a60acda5f5c844410860ab
Qinwen Yan <769423645@qq.com>
2026-08-12 04:00:28 +0000

    update the the whole header file

--- src/include/zookeeperutil.h
+++ src/include/zookeeperutil.h
@@ -1,7 +1,23 @@
-#pragma once 
+#pragma once
 
 #include <semaphore.h>
 #include <zookeeper/zookeeper.h>
+
 #include <string>
 
 //封装的zk客户端类
+class ZkClient {
+ public:
+  ZkClient() = default;
+  ~ZkClient();
+  // zkclient启动链接zkserver
+  void Start();
+  //在zkserver上根据指定的path创建znode节点
+  // 默认state为0就是永久性节点
+  void Create(const char* path, const char* data, int datalen, int state = 0);
+  //根据指定znode
+  std::string GetData(const char* path);
+private:
+//zk的客户端句柄，通过句柄来操作zk server
+zhandle_t* mZhandle; 
+};
