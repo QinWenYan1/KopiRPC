fff187a66b4463cc77e20193a40e28076d886dac
Qinwen Yan <769423645@qq.com>
2026-08-14 10:39:29 +0000

    update the auto build file

--- autobuild.sh
+++ autobuild.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
+# !/bin/bash
+# 需要给别人提供头文件作为API
+# 其次将 静态库 + 头文件 都放在lib这个文件夹之下
 
+set -e
+
+rm -rf `pwd`/build/* #先清理
+cd `pwd`/build && 
+    cmake .. &&
+    make 
+cd ..
+
+cp -r `pwd`/src/include `pwd`/lib #拷贝过去
