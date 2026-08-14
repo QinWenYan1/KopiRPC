ccf419c56ec612e931c63337066796a9c1101ff7
Qinwen Yan <769423645@qq.com>
2026-08-14 10:42:36 +0000

    rewrite the autobuild file

--- autobuild.sh
+++ autobuild.sh
@@ -1,6 +1,9 @@
-# !/bin/bash
+#!/bin/bash
 # 需要给别人提供头文件作为API
 # 其次将 静态库 + 头文件 都放在lib这个文件夹之下
+# 如何使用build之后的产物: 
+#   1. /include 就放在 其他项目的 user include 下
+#   2. libkopirpc.a 放在 user lib 下
 
 set -e
 
