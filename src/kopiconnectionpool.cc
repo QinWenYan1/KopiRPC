d41aa2edfbd6314b12295efd4f2bbebe748abaae
Qinwen Yan <769423645@qq.com>
2026-08-21 07:17:30 +0000

    update the implementation of the kopiconnectpool

--- src/kopiconnectionpool.cc
+++ src/kopiconnectionpool.cc
@@ -1,7 +1,10 @@
 #include "KopiConnectpool.h"
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
+#include <mutex>
 
 // 假设每个 IP:Port 最多简历 1024 个长链接 （必须是 2 的幂）
 static const int MAX_CONN_SIZE = 1024; 
@@ -35,17 +38,50 @@ KopiConnectPool::~KopiConnectPool(){
 int KopiConnectPool::BorrowConnection(const std::string& ip, uint16_t port){
     std::string key = ip + ":" + std::to_string(port); 
     ConnectionBucket* bucket = nullptr; 
-    std::lock_guard<std::mutex> lock(globalMtx); 
-    auto it = pools.find(key); 
-    if (it == pools.end()){
-        bucket = new ConnectionBucket(MAX_CONN_SIZE); 
-        pools[key] = bucket; 
-    } else{
-        bucket = it->second; 
+
+    // 1.   
+    // 查询全局 map 找到对应的 bucket
+    // 值在查找和创建 bucket 时加全局锁，粒度及其小
+    {
+        std::lock_guard<std::mutex> lock(globalMtx); 
+        if (pools.find(key) == pools.end()){
+            //没有找到创建新的bucket
+            pools[key] = new ConnectionBucket(); 
+        }
+        bucket = pools[key]; 
+    }
+
+    // 2. 对当前节点的bucket加锁
+    std::unique_lock<std::mutex> lock(bucket->mtx);
+
+    // 情况 A: 如果有现成空闲长链接，直接出队返回，性能最高
+    if (!bucket->freeFds.empty()){
+        int fd = bucket->freeFds.front(); 
+        bucket->freeFds.pop(); 
+        return fd; 
+    }
+
+    //情况 B: 没有空闲的链接，并且当前链接的链接数量还咩有达到 1024 上限，允许新建链接
+    if (bucket->active_count < maxConnPerNode){
+        bucket->active_count++; //预占位
+
+        lock.unlock(); //关键设计：在进行耗时的 connect 系统调用前必须解锁，避免阻塞其他线程
+        // IPv4地址，面向链接的字节流 socket ， 并让操作系统根据前面的参数选择合适的协议
+        int fd = socket(AF_INET,SOCK_STREAM,0); 
+
+        // 核心优化：禁止 TCP nagle 算法
+        // 小数据包不等待，直接发出，延迟大幅度降低
+        int opt = 1; 
+        setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt));
+
+        struct sockaddr_in addr; 
+        addr.sin_family = AF_INET; 
+        addr.sin_port = htons(port); 
+        addr.sin_addr.s_addr = inet_addr(ip.c_str()); 
     }
 
     int fd = -1; 
     // 优先从无锁队列获取
     return -1; 
-    
+
 }
